970227c330a13afd23df9e8669fe74bf7277b4b4
jaime <garciadiazjaime@gmail.com>
2020-09-03 22:14:12 -0500

    typos

--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -21,7 +21,7 @@
 	<h2>¿Está cerrada la frontera en Tijuana?</h2>
 	<p>
 		La frontera seguirá cerrada para viajes no esenciales (principalmente turistas) hasta Septiembre 21, 2020.
-		<br />
+		<br /><br />
 		La fecha se ha ido extendiendo mes tras mes, así que seguramente se seguirá posponiendo.
 		<br />
 		<br />
@@ -33,10 +33,8 @@
 	<h2>¿Puedo cruzar si mi pasaporte estadounidense está vencido?</h2>
 	<p>
 		Si eres ciudadano estadounidense y piensas cruzar la frontera necesitas tener tu pasaporte vigente.
-		<br />
-		Si estas en Tijuana puedes acudir al consulado.
-		<br />
-		Página del consulado americano en Tijuana:
+		<br /><br />
+		Si estas en Tijuana puedes acudir al consulado. Página del consulado americano en Tijuana:
 		<br />
 		<a href="https://mx.usembassy.gov/embassy-consulates/tijuana/" target="_blank" rel="nofollow">US Embassy</a>
 
@@ -51,6 +49,7 @@
 	<p>
 		Tanto para carros como personas, la garita está abierta las 24hrs.
 		<br />
+		<br />
 		Sin embargo para cruce comercial (principalmente tractores) solo de 6am a 8pm.
 		<br />
 		<br />
@@ -59,7 +58,7 @@
 		<a href="https://www.cbp.gov/" target="_blank" rel="nofollow">CBP</a>
 	</p>
 
-	<h2>¿Cuánto tiempo hay de espera en la gartia de San Ysidro / Otay?</h2>
+	<h2>¿Cuánto tiempo hay de espera en la garita de San Ysidro / Otay?</h2>
 	<p>
 		La información del tiempo de espera en garitas la puedes encontrar en nuestra página principal:
 		<br />
@@ -69,18 +68,16 @@
 
 	<h2>¿Es cierto que no hay paso de California a Tijuana?</h2>
 	<p>
-		La recomendación por parte de las entidades americanas en de evitar cruzar a México. 
-		<br />
-		Sin embargo la frontera para fruzar a México se encuentra abierta. Así sí se puede cruzar de California a Tijuana.
-
-		<br />
-		Solo recuerda que para el regreso necesitas tener tu pasoporte vigente.
+		La recomendación por parte de las autoridades americanas es de evitar cruzar a México. 
+		<br /><br />
+		Sin embargo la frontera para cruzar a México se encuentra abierta. Así que sí se puede cruzar de California a Tijuana.
+		<br /><br />
+		Solo recuerda que para el regreso necesitas tener tu pasaporte vigente.
 
 		<br />
 		<br />
 		Fuente:
 		<br />
-		
 		<a href="https://mx.usembassy.gov/travel-restrictions-fact-sheet/?fbclid=IwAR0onjNbmzZ4OsDrrbX1QpzTRP4lTT64xavRbnm706y7kuE9x9mWM6iNq54" target="_blank" rel="nofollow">US Embassy</a>
 	</p>
 </section>
